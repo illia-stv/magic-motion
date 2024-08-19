392fcf406725f26a0094a4ef0400b2e1418cc902
Illia Sheremetov <ilya@sheremetov.com>
2024-08-19 21:23:12 +0200

    Revert "Fix config."
    This reverts commit b0429b915688a7732ac97bdcacbb54b04c97af0c.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
     url: 'https://illia-stv.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/magic-motion',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
