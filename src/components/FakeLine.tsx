9b384e2421900e17067641f7cf042ccfa6f7f5fb
Illia Sheremetov <ilya@sheremetov.com>
2024-08-19 21:26:40 +0200

    Deploy website - based on 392fcf406725f26a0094a4ef0400b2e1418cc902

--- src/components/FakeLine.tsx
+++
@@ -1,5 +0,0 @@
-const FakeLine = ({ height }: { height: number }) => {
-    return <span style={{ height, display: 'block' }} />;
-};
-
-export default FakeLine;
--- src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as MagicMotion } from './components/MagicMotion';
-export type { default as MagicMotionConfig } from './components/MagicMotion';
