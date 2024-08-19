b9472a315938181ef7256668ba779be9eb6915de
Illia Sheremetov <ilya@sheremetov.com>
2024-08-19 21:47:55 +0200

    Fix config.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/illia_stv/magic-motion/tree/main/docs/',
+                        'https://github.com/illia-stv/magic-motion/tree/main/docs/',
                 },
                 theme: {
                     customCss: './src/css/custom.css',
